b50c6435f95cd6cfc0d5fcb95cd45b8a85add1a0
Ozan Demir <ozan2005@icloud.com>
2024-02-20 15:03:49 -0500

    created methods and tests in main

--- src/Main.java
+++ src/Main.java
@@ -11,10 +11,13 @@ public class Main {
             }
         }
 
+        //test for 5 hello worlds
         RecursionMethods.helloWorldRepeat(5);
 
+        //test between 1 and 1000
         System.out.println(RecursionMethods.sumOfSevens(1,1000));
 
+        //test array
         int[] arr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
         //test all numbers in array including two out of bounds: -1 and 11
